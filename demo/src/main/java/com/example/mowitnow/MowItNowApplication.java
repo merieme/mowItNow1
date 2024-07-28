358c210a238e60a8aa2a4f1680ab55a41a9aec05
merieme <elmarzouki.merieme@gmail.Com>
2024-07-28 18:36:55 +0200

    project

--- demo/src/main/java/com/example/mowitnow/MowItNowApplication.java
+++ demo/src/main/java/com/example/mowitnow/MowItNowApplication.java
@@ -1,8 +1,8 @@
-
 package com.example.mowitnow;
-// MowItNowApplication.java
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,22 +11,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MowItNowApplication {
+public class
+MowItNowApplication {
 
-	@Autowired
-	JobLauncher jobLauncher;
+    @Autowired
+    JobLauncher jobLauncher;
 
-	@Autowired
-	Job job;
+    @Autowired
+    Job job;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MowItNowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MowItNowApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			jobLauncher.run(job, new JobParameters());
-		};
-	}
+    @Bean
+    public CommandLineRunner run(JobLauncher jobLauncher, Job job) {
+        return args -> {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("input.file.path",
+                            "input.txt")
+                    .toJobParameters();
+            jobLauncher.run(job, jobParameters);
+        };
+    }
 }
